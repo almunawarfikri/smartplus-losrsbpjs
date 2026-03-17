01a13a910ef08cc9843ce86cef09891292b61090
Fikri <almunawarfikri@gmail.com>
2026-03-17 15:12:03 +0700

    Update auto-display-100.user.js

--- auto-display-100.user.js
+++ auto-display-100.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Auto Display 100 E-Ranap Smartplus
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Set show entries jadi 100 otomatis
 // @match        http://192.168.3.16/smartplus/erm_ranap*
+// @match        http://192.168.3.16/smartplus/nurse_station/eranap*
 // @updateURL    https://raw.githubusercontent.com/almunawarfikri/smartplus-tools/main/auto-display-100.user.js
 // @downloadURL  https://raw.githubusercontent.com/almunawarfikri/smartplus-tools/main/auto-display-100.user.js
 // @grant        none
